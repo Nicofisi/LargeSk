678454441deb99a574598a0480ce32b907584ca0
Nicofisi <m.lomiak@gmail.com>
2016-03-16 12:38:45 +0100

    Metrics and config

--- src/pl/pickaxe/largesk/LargeSk.java
+++ src/pl/pickaxe/largesk/LargeSk.java
@@ -1,6 +1,13 @@
 package pl.pickaxe.largesk;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import org.bukkit.Server;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -22,18 +29,36 @@ import pl.pickaxe.largesk.aac.ExprAacPing;
 import pl.pickaxe.largesk.aac.ExprAacTps;
 import pl.pickaxe.largesk.aac.ExprViolationLevel;
 import pl.pickaxe.largesk.util.EnumClassInfo;
+import pl.pickaxe.largesk.util.Metrics;
 
 public class LargeSk extends JavaPlugin {
+	
+	public static File configf;
+    public static FileConfiguration config;
 	
 	@Override
 	
 	public void onEnable() {
+		
+		//Enabling timer
 		long eTime = System.currentTimeMillis();
 		Server s = getServer();
 		
+		//Configs
+		configf = new File(getDataFolder(), "config.yml");
+		if ( ! configf.exists())
+		{
+			getLogger().info("The config.yml file does not exist or is outdated.");
+			getLogger().info("I'll copy it from the plugin's jar file now.");
+			configf.getParentFile().mkdirs();
+			copy(getResource("config.yml"), configf);
+		}
+		
+		
+		//Registring Skript addon
 		Skript.registerAddon(this);
 		
-		//Different Stuff
+		//Registring hacktype enum from AAC
 		EnumClassInfo.create(HackType.class, "hacktype").register();
 		
 		//Learning
@@ -70,13 +95,51 @@ public class LargeSk extends JavaPlugin {
 			Skript.registerExpression(ExprSkinOfPlayer.class, String.class, ExpressionType.PROPERTY, "skin of %player%","%player%'s skin");
 		}
 		
+		//Metrics
+		if (getConfig().getBoolean("enableMetrics"))
+		{
+			try {
+				Metrics metrics = new Metrics(this);
+				metrics.start();
+			}
+			catch (IOException e)
+			{
+				getLogger().warning("Enabling Metrics failed!");
+			}
+		}
+		else
+		{
+			getLogger().info("Metrics are disabled, so sorry to hear that :(");
+		}
+		
+		//Announcing how much time enabling took
 		eTime = System.currentTimeMillis() - eTime;
 		getLogger().info("Enabling completed, took " + eTime + "ms.");
 	}
+	
+	//On disable
 	@Override
 	public void onDisable() {
 		getLogger().info("Bye, Senpai!");
 	}
-	
-
+	//Some stuff to copy the default config file from the plugin jar file.
+	public void copy(InputStream in, File file) {
+		try {
+			OutputStream out = new FileOutputStream(file);
+			byte[] buf = new byte[1024];
+			int len;
+			while ((len = in.read(buf)) > 0) {
+				out.write(buf, 0, len);
+			}
+			out.close();
+			in.close();
+		}
+		catch (Exception e)
+		{
+			//It shouldn't happen
+			getLogger().severe("Mate, I really tried to copy the default config, but I could not.");
+			getLogger().severe("Report it on https://github.com/Nicofisi/LargeSk/issues please.");
+			e.printStackTrace();
+		}
+	}
 }
